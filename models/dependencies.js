fdd6fcf2cebb663cc713360e4f04287f8c635635
AnaLuiza <analuizalopes.oliveira@gmail.com>
2020-10-01 19:02:55 -0300

    commit do odio

---
+++ models/dependencies.js
@@ -0,0 +1,9 @@
+exports.express = require('express');
+exports.mongoose = require('mongoose');
+exports.passport = require('passport');
+exports.session = require('express-session');
+exports.bodyParser = require('body-parser');
+exports.connectMongo = require('connect-mongo');
+exports.passportSpotify = require('passport-spotify');
+exports.qs = require('querystring');
+exports.path = require('path');
